ce7256439307bfb1f73a0d5f0f8695e3be62f628
littlesaturnalien <kmryfonseca@uamv.edu.ni>
2025-04-07 17:08:01 -0600

    Cors policy included

---
+++ backend/ICorte-POO-Project/src/main/java/org/kmryfv/icortepooproject/config/WebConfig.java
@@ -0,0 +1,19 @@
+package org.kmryfv.icortepooproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://172.16.12.18:3000", "http://localhost:3000", "http://172.16.190.6:3000/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
